236f99cb4cd2580ce68061171e9e049c1042fcf3
Boris Serebrov <serebrov@gmail.com>
2013-05-12 18:22:12 +0300

    Add instrouctions to setup gnome and desktop/session files

--- setup-gnome.sh
+++ setup-gnome.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
 # see
-# https://faq.i3wm.org/question/346/how-can-i-use-i3-in-gnome-2-de/
 # http://blog.hugochinchilla.net/2013/03/using-gnome-3-with-i3-window-manager/
+# https://faq.i3wm.org/question/346/how-can-i-use-i3-in-gnome-2-de/
 # http://askubuntu.com/questions/26115/i3-wm-with-auto-internet-connectivity-and-a-gnome-panel
 
-gconftool-2 -s /desktop/gnome/session/required_components/windowmanager i3 --type string
-gconftool-2 -s /apps/nautilus/preferences/show_desktop false --type boolean
+#gconftool-2 -s /desktop/gnome/session/required_components/windowmanager i3 --type string
+#gconftool-2 -s /apps/nautilus/preferences/show_desktop false --type boolean
+
+#gsettings set org.gnome.desktop.background show-desktop-icons false
+
+#remove bottom panel
+# run dconf-editor
+# org - gnome - gnome-panel - layout
+# change ['top-panel','bottom-panel'] - ['top-panel']
+
+#auto hide top panel
+# run dconf-editor
+# org - gnome - gnome-panel - layout - toplevels - top-panel
+# change auto-hide
+
+cp gnome-i3.desktop /usr/share/xsessions/gnome-i3.desktop
+cp gnome-i3.session /usr/share/gnome-session/gnome-i3.session
