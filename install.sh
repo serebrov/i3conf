883ccdc508f8d137690e7bb0ac9e513297ff5995
Boris Serebrov <serebrov@gmail.com>
2019-10-27 15:49:04 +0200

    Add matrix

--- install.sh
+++ install.sh
@@ -303,3 +303,6 @@ sudo apt-get install -y keepassxc
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 # Show current value
 cat /proc/sys/fs/inotify/max_user_watches
+
+sudo apt -y install pv
+clear ; for t in "Wake up" "The Matrix has you" "Follow the white rabbit" "Knock, knock";do clear;pv -qL10 <<<$'\e[2J'$'\e[32m'$t$'\e[37m';sleep 5;done
